e7eeb047c161a2a8261bfb7a3ba4b33dd7425f32
sum01 <sum01@protonmail.com>
2018-04-24 01:56:59 -0400

    Big edits to make Eslint happy
    Unnested some functions that didn't need to be nested.
    Removed a bunch of pointless logic & if-checks.
    Used const as much as possible.
    
    There should be better speed when running the calculation. Usually the 'Calculate' btn isn't even visiblly disabled since it's quicker.
    
    Probably still need to fix the Regexp issues on Codacy though, but I don't see them locally for whatever reason...

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /*
-  This js performs the API calls & fills the boxes for displaying network/money info about Vcash.
-  The goal of this is to make all API calls originate from the user, and not require some external server.
+	This js performs the API calls & fills the boxes for displaying network/money info about Vcash.
+	The goal of this is to make all API calls originate from the user, and not require some external server.
 */
 
 function query(api_name, target_api) {
@@ -30,17 +30,18 @@ function query(api_name, target_api) {
 
 	// JSON.parse() wasn't working, so a number parser is fine for our purposes
 	function parser(data) {
+		// double
 		let output = 0.0;
 		// Split by the commas
-		let array = data.split(",");
+		const array = data.split(",");
 		// Regex to match either a full number, or a number with a decimal
-		let api_regex = new RegExp("([0-9]+(.[0-9]+)?)", "g");
+		const api_regex = new RegExp("([0-9]+(.[0-9]+)?)", "g");
 
 		// Test if the target api matches the bittrex call
 		if (api_name === "bittrex") {
 			// High is on 3, Low is on 4
-			let high = parseFloat(array[3].match(api_regex));
-			let low = parseFloat(array[4].match(api_regex));
+			const high = parseFloat(array[3].match(api_regex));
+			const low = parseFloat(array[4].match(api_regex));
 			// Returns the average of the two
 			output = (high + low) / 2.0;
 		} else if (api_name === "explorer") {
@@ -93,28 +94,18 @@ function format_for_display(data, precision) {
 	data = parseFloat(data);
 
 	// Credit https://stackoverflow.com/a/2901298
-	function format_commas(x) {
-		let parts = x.toString().split(".");
+	function format_commas(str) {
+		let parts = str.toString().split(".");
 		parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 		return parts.join(".");
 	}
 
 	// Is money
 	if (precision === 2) {
-		let is_negative = false;
 		// Check (and remember) if negative
-		if (data < 0.0) {
-			is_negative = true;
-			// Removes sign
-			data = Math.abs(data);
-		}
-
-		let dollar_symbol = "$";
-
-		if (is_negative) {
-			dollar_symbol = `-${dollar_symbol}`;
-		}
-
+		const dollar_symbol = data < 0.0 ? "-$" : "$";
+		// Removes a negative sign, if any
+		data = Math.abs(data);
 		data = format_commas(data.toFixed(precision));
 
 		return `${dollar_symbol}${data}`;
@@ -130,10 +121,10 @@ function format_for_display(data, precision) {
 
 function hps_multiplier(hps) {
 	/*
-    Multiply hashrate to get the hashrate as base hashrate, instead of selected hash per second.
+		Multiply hashrate to get the hashrate as base hashrate, instead of selected hash per second.
 
-    Values from https://en.wikipedia.org/wiki/Template:Bitrates
-  */
+		Values from https://en.wikipedia.org/wiki/Template:Bitrates
+	*/
 
 	switch (hps) {
 		case "h":
@@ -167,10 +158,12 @@ function fill_network_badges() {
 			document.getElementById(
 				"pow_difficulty"
 			).textContent = format_for_display(values[0], 0);
+
 			document.getElementById("block_count").textContent = format_for_display(
 				values[1],
 				0
 			);
+
 			document.getElementById(
 				"network_hashrate"
 			).textContent = format_for_display(values[2], 0);
@@ -182,360 +175,344 @@ function fill_network_badges() {
 		});
 }
 
-// Prevent clicking button till the query ends.
-function disable_btn(button, disable) {
-	button.disabled = disable;
-	if (disable) {
-		button.textContent = "Loading...";
-	} else {
-		button.textContent = "Calculate";
-	}
-}
+// NOTE: Always pass parseFloat() so toFixed doesn't break, even if 0 precision.
+function fill_grid_elements(target_element, value, precision) {
+	/*
+				Id's of various html tags are "X_Y"
 
+				Where X is profit/mined/power
+				and Y is day/week/month/year
+
+				Ex: The id for power cost per month is "power_month"
+				*/
+
+	let display_value = value;
+	document.getElementById(
+		target_element + "_day"
+	).textContent = format_for_display(display_value, precision);
+	display_value = value * 7.0;
+
+	document.getElementById(
+		target_element + "_week"
+	).textContent = format_for_display(display_value, precision);
+	display_value = value * 7.0 * 3.0;
+
+	document.getElementById(
+		target_element + "_month"
+	).textContent = format_for_display(display_value, precision);
+	display_value = value * 7.0 * 3.0 * 12.0;
+
+	document.getElementById(
+		target_element + "_year"
+	).textContent = format_for_display(display_value, precision);
+}
+// Credit to @whphhg as this is just a slightly edited version of his Node.js code
+function get_pow_reward(block) {
+	function total_reward() {
+		let subsidy = 0;
+
+		if (block >= 136400 && block <= 136400 + 1000) {
+			subsidy = 1;
+		} else {
+			subsidy = 128 * 1000000;
+
+			if (block < 325000) {
+				for (let i = 50000; i <= block; i += 50000) {
+					subsidy -= subsidy / 6;
+				}
+			} else if (block < 385000) {
+				for (let i = 10000; i <= block; i += 10000) {
+					subsidy -= subsidy / 28;
+					subsidy = Math.ceil(subsidy);
+					subsidy -= subsidy / 28 * 4 / 28;
+					subsidy = Math.ceil(subsidy);
+				}
+			} else {
+				for (let i = 7000; i <= block; i += 7000) {
+					subsidy -= subsidy / 28;
+					subsidy = Math.ceil(subsidy);
+					subsidy -= subsidy / 28 * 4 / 28;
+					subsidy = Math.ceil(subsidy);
+				}
+			}
+
+			if (subsidy / 1000000 < 1) {
+				subsidy = 1;
+				subsidy *= 1000000;
+			}
+		}
+
+		subsidy /= 1000000;
+		console.log(`Using total PoW+PoS reward = ${subsidy}`);
+		return subsidy;
+	}
+
+	function incentive_percent() {
+		const percents = [
+			{
+				block: 210000,
+				percent: 1
+			},
+			{
+				block: 220000,
+				percent: 2
+			},
+			{
+				block: 220222,
+				percent: 3
+			},
+			{
+				block: 220888,
+				percent: 4
+			},
+			{
+				block: 221998,
+				percent: 5
+			},
+			{
+				block: 223552,
+				percent: 6
+			},
+			{
+				block: 225550,
+				percent: 7
+			},
+			{
+				block: 227992,
+				percent: 8
+			},
+			{
+				block: 230878,
+				percent: 9
+			},
+			{
+				block: 234208,
+				percent: 10
+			},
+			{
+				block: 237982,
+				percent: 11
+			},
+			{
+				block: 242200,
+				percent: 12
+			},
+			{
+				block: 246862,
+				percent: 13
+			},
+			{
+				block: 251968,
+				percent: 14
+			},
+			{
+				block: 257518,
+				percent: 15
+			},
+			{
+				block: 263512,
+				percent: 16
+			},
+			{
+				block: 269950,
+				percent: 17
+			},
+			{
+				block: 276832,
+				percent: 18
+			},
+			{
+				block: 284158,
+				percent: 19
+			},
+			{
+				block: 291928,
+				percent: 20
+			},
+			{
+				block: 300142,
+				percent: 21
+			},
+			{
+				block: 308800,
+				percent: 22
+			},
+			{
+				block: 317902,
+				percent: 23
+			},
+			{
+				block: 327448,
+				percent: 24
+			},
+			{
+				block: 337438,
+				percent: 25
+			},
+			{
+				block: 347872,
+				percent: 26
+			},
+			{
+				block: 358750,
+				percent: 27
+			},
+			{
+				block: 370072,
+				percent: 28
+			},
+			{
+				block: 381838,
+				percent: 29
+			},
+			{
+				block: 394048,
+				percent: 30
+			},
+			{
+				block: 406702,
+				percent: 31
+			},
+			{
+				block: 419800,
+				percent: 32
+			},
+			{
+				block: 433342,
+				percent: 33
+			},
+			{
+				block: 447328,
+				percent: 34
+			},
+			{
+				block: 461758,
+				percent: 35
+			},
+			{
+				block: 476632,
+				percent: 36
+			},
+			{
+				block: 491950,
+				percent: 37
+			},
+			{
+				block: 507712,
+				percent: 38
+			},
+			{
+				block: 523918,
+				percent: 39
+			},
+			{
+				block: 540568,
+				percent: 40
+			}
+		];
+
+		const percents_len = percents.length - 1;
+
+		if (block >= percents[percents_len].block) {
+			console.log(
+				`Using incentive percent = ${percents[percents_len].percent}`
+			);
+			return percents[percents_len].percent;
+		} else {
+			for (let obj of percents) {
+				if (block < obj.block) {
+					console.log(`Using incentive percent = ${obj.percent - 1}`);
+					return obj.percent - 1;
+				}
+			}
+		}
+	}
+
+	const reward_total = total_reward();
+	const incentive_reward = reward_total / 100 * incentive_percent();
+	// Returns the PoW-only reward
+	console.log(`Using PoW reward = ${reward_total - incentive_reward}`);
+	return parseFloat(reward_total - incentive_reward);
+}
 // Triggered by onclick() from calculate button
+// The eslint comment is needed to avoid a non-issue
+// eslint-disable-next-line no-unused-vars
 function main() {
+	/*
+		List of DOM ID's...
+
+		Hashrate id="hashrate"
+		Hashrate per second id="hash_per_sec"
+		Power consumption id="power_consumption"
+		Power cost id="power_cost"
+
+		Pre-parse all input to float to avoid numbers getting rounded into the abyss
+	*/
+	// Convert DOWN to the base H/s by multiplying
+	const hashrate =
+		parseFloat(document.getElementById("hashrate").value) *
+		hps_multiplier(document.getElementById("hash_per_sec").value);
+	const power_consumption = parseFloat(
+		document.getElementById("power_consumption").value
+	);
+	const power_cost = parseFloat(document.getElementById("power_cost").value);
+
+	// Safety-check the user input
+	// If we didn't get numeric inputs, thrown a warning & quit early
+	if (isNaN(hashrate) || isNaN(power_cost) || isNaN(power_consumption)) {
+		console.warn("Incorrect value(s) in input boxes!");
+		return 1;
+	}
+
 	// Disable/enable button while running
 	let calculate_button = document.getElementById("calculate_btn");
-	disable_btn(calculate_btn, true);
+	// Disable the calc button while running calulations & queries
+	calculate_button.disabled = true;
+	calculate_button.textContent = "Loading...";
 
-	/*
-    Hashrate id="hashrate"
-    Hashrate per second id="hash_per_sec"
-    Power consumption id="power_consumption"
-    Power cost id="power_cost"
-  */
-	let hashrate = document.getElementById("hashrate").value;
-	let power_consumption = document.getElementById("power_consumption").value;
-	let power_cost = document.getElementById("power_cost").value;
+	// Chain promises together because some of the math depends on eachother
+	Promise.all([
+		query("explorer", "getnetworkhashps"),
+		query("bittrex", "getmarketsummary?market=BTC-XVC"),
+		query("bittrex", "getmarketsummary?market=USDT-BTC"),
+		query("explorer", "getblockcount")
+	])
+		.then(values => {
+			// Values is an array with the results of each promise
 
-	// Fail if empty string ( <input type="number"> returns an empty string if not a number)
-	// Fail if not positive values (to avoid pointless calculations/api calls) | Letting power cost be 0 because sometimes people have free power.
-	if (
-		hashrate !== " " &&
-		power_consumption !== " " &&
-		power_cost !== " " &&
-		hashrate > 0 &&
-		power_consumption > 0 &&
-		power_cost >= 0
-	) {
-		// Pre-parse to float to avoid dealing with incorrect num-types
-		hashrate = parseFloat(hashrate);
-		power_consumption = parseFloat(power_consumption);
-		power_cost = parseFloat(power_cost);
+			// HashRate/24h_Average_Net_HashRate*PoW_Reward*18*24  = XVC/DAY
+			const avg_mined =
+				hashrate /
+				parseFloat(values[0]) *
+				get_pow_reward(parseInt(values[3])) *
+				18.0 *
+				24.0;
 
-		// Convert DOWN to the base H/s by multiplying
-		hashrate *= hps_multiplier(document.getElementById("hash_per_sec").value);
+			// power_consumption / 1000 is to convert watts to kilowatts
+			// power_cost is in kWh
+			// * 24 to convert to cost per day
+			const avg_power_cost = power_consumption / 1000.0 * power_cost * 24.0;
+			// Calc how many bitcoins you'd make, then multiply it by the conversion to USD
+			// (XVC-to-BTC * mined) * BTC-to-USDT
+			// Minus power cost from profit to get actual profit
+			const avg_profit =
+				parseFloat(values[1]) * avg_mined * parseFloat(values[2]) -
+				avg_power_cost;
 
-		// NOTE: Always pass parseFloat() so toFixed doesn't break, even if 0 precision.
-		function fill_grid_elements(target_element, value, precision) {
-			/*
-        Id's of various html tags are "X_Y"
+			// Fill all at once to appear less laggy
+			fill_grid_elements("mined", avg_mined, 8);
+			fill_grid_elements("profit", avg_profit, 2);
+			fill_grid_elements("power", avg_power_cost, 2);
+		})
+		.catch(function(error) {
+			// Throw an alert if query fails
+			console.error(error);
+			alert(`API query failed!\n\n${error}`);
+		});
 
-        Where X is profit/mined/power
-        and Y is day/week/month/year
-
-        Ex: The id for power cost per month is "power_month"
-      */
-
-			let display_value = value;
-			document.getElementById(
-				target_element + "_day"
-			).textContent = format_for_display(display_value, precision);
-			display_value = value * 7.0;
-			document.getElementById(
-				target_element + "_week"
-			).textContent = format_for_display(display_value, precision);
-			display_value = value * 7.0 * 3.0;
-			document.getElementById(
-				target_element + "_month"
-			).textContent = format_for_display(display_value, precision);
-			display_value = value * 7.0 * 3.0 * 12.0;
-			document.getElementById(
-				target_element + "_year"
-			).textContent = format_for_display(display_value, precision);
-		}
-
-		// Credit to @whphhg as this is just a slightly edited version of his Node.js code
-		function get_pow_reward(block) {
-			function total_reward() {
-				let subsidy = 0;
-
-				if (block >= 136400 && block <= 136400 + 1000) {
-					subsidy = 1;
-				} else {
-					subsidy = 128 * 1000000;
-
-					if (block < 325000) {
-						for (let i = 50000; i <= block; i += 50000) {
-							subsidy -= subsidy / 6;
-						}
-					} else if (block < 385000) {
-						for (let i = 10000; i <= block; i += 10000) {
-							subsidy -= subsidy / 28;
-							subsidy = Math.ceil(subsidy);
-							subsidy -= subsidy / 28 * 4 / 28;
-							subsidy = Math.ceil(subsidy);
-						}
-					} else {
-						for (let i = 7000; i <= block; i += 7000) {
-							subsidy -= subsidy / 28;
-							subsidy = Math.ceil(subsidy);
-							subsidy -= subsidy / 28 * 4 / 28;
-							subsidy = Math.ceil(subsidy);
-						}
-					}
-
-					if (subsidy / 1000000 < 1) {
-						subsidy = 1;
-						subsidy *= 1000000;
-					}
-				}
-
-				subsidy /= 1000000;
-				console.log(`Using total PoW+PoS reward = ${subsidy}`);
-				return subsidy;
-			}
-
-			function incentive_percent() {
-				const percents = [
-					{
-						block: 210000,
-						percent: 1
-					},
-					{
-						block: 220000,
-						percent: 2
-					},
-					{
-						block: 220222,
-						percent: 3
-					},
-					{
-						block: 220888,
-						percent: 4
-					},
-					{
-						block: 221998,
-						percent: 5
-					},
-					{
-						block: 223552,
-						percent: 6
-					},
-					{
-						block: 225550,
-						percent: 7
-					},
-					{
-						block: 227992,
-						percent: 8
-					},
-					{
-						block: 230878,
-						percent: 9
-					},
-					{
-						block: 234208,
-						percent: 10
-					},
-					{
-						block: 237982,
-						percent: 11
-					},
-					{
-						block: 242200,
-						percent: 12
-					},
-					{
-						block: 246862,
-						percent: 13
-					},
-					{
-						block: 251968,
-						percent: 14
-					},
-					{
-						block: 257518,
-						percent: 15
-					},
-					{
-						block: 263512,
-						percent: 16
-					},
-					{
-						block: 269950,
-						percent: 17
-					},
-					{
-						block: 276832,
-						percent: 18
-					},
-					{
-						block: 284158,
-						percent: 19
-					},
-					{
-						block: 291928,
-						percent: 20
-					},
-					{
-						block: 300142,
-						percent: 21
-					},
-					{
-						block: 308800,
-						percent: 22
-					},
-					{
-						block: 317902,
-						percent: 23
-					},
-					{
-						block: 327448,
-						percent: 24
-					},
-					{
-						block: 337438,
-						percent: 25
-					},
-					{
-						block: 347872,
-						percent: 26
-					},
-					{
-						block: 358750,
-						percent: 27
-					},
-					{
-						block: 370072,
-						percent: 28
-					},
-					{
-						block: 381838,
-						percent: 29
-					},
-					{
-						block: 394048,
-						percent: 30
-					},
-					{
-						block: 406702,
-						percent: 31
-					},
-					{
-						block: 419800,
-						percent: 32
-					},
-					{
-						block: 433342,
-						percent: 33
-					},
-					{
-						block: 447328,
-						percent: 34
-					},
-					{
-						block: 461758,
-						percent: 35
-					},
-					{
-						block: 476632,
-						percent: 36
-					},
-					{
-						block: 491950,
-						percent: 37
-					},
-					{
-						block: 507712,
-						percent: 38
-					},
-					{
-						block: 523918,
-						percent: 39
-					},
-					{
-						block: 540568,
-						percent: 40
-					}
-				];
-
-				const percents_len = percents.length - 1;
-
-				if (block >= percents[percents_len].block) {
-					console.log(
-						`Using incentive percent = ${percents[percents_len].percent}`
-					);
-					return percents[percents_len].percent;
-				} else {
-					for (let i in percents) {
-						if (block < percents[i].block) {
-							console.log(
-								`Using incentive percent = ${percents[i].percent - 1}`
-							);
-							return percents[i].percent - 1;
-						}
-					}
-				}
-			}
-
-			let reward_total = total_reward();
-			let incentive_reward = reward_total / 100 * incentive_percent();
-			// Returns the PoW-only reward
-			console.log(`Using PoW reward = ${reward_total - incentive_reward}`);
-			return parseFloat(reward_total - incentive_reward);
-		}
-
-		// Chain promises together because some of the math depends on eachother
-		Promise.all([
-			query("explorer", "getnetworkhashps"),
-			query("bittrex", "getmarketsummary?market=BTC-XVC"),
-			query("bittrex", "getmarketsummary?market=USDT-BTC"),
-			query("explorer", "getblockcount")
-		])
-			.then(values => {
-				// Values is an array with the results of each promise
-
-				// HashRate/24h_Average_Net_HashRate*PoW_Reward*18*24  = XVC/DAY
-				let avg_mined =
-					hashrate /
-					parseFloat(values[0]) *
-					get_pow_reward(parseInt(values[3])) *
-					18.0 *
-					24.0;
-
-				// Calc how many bitcoins you'd make, then multiply it by the conversion to USD
-				// (XVC-to-BTC * mined) * BTC-to-USDT
-				let avg_profit =
-					parseFloat(values[1]) * avg_mined * parseFloat(values[2]);
-				// power_consumption / 1000 is to convert watts to kilowatts
-				// power_cost is in kWh
-				// * 24 to convert to cost per day
-				let avg_power_cost = power_consumption / 1000.0 * power_cost * 24.0;
-				// Minus power cost from profit to get actual profit
-				avg_profit -= avg_power_cost;
-
-				// Fill all at once to appear less laggy
-				fill_grid_elements("mined", avg_mined, 8);
-				fill_grid_elements("profit", avg_profit, 2);
-				fill_grid_elements("power", avg_power_cost, 2);
-
-				// Re-enable button
-				disable_btn(calculate_btn, false);
-			})
-			.catch(function(error) {
-				// Throw an alert if query fails
-				console.error(error);
-				alert(`API query failed!\n\n${error}`);
-
-				// Re-enable button
-				disable_btn(calculate_btn, false);
-			});
-	} else {
-		console.warn("Incorrect value(s) in input boxes!");
-		// Re-enable button
-		disable_btn(calculate_btn, false);
-	}
+	// Re-enable button
+	calculate_button.disabled = false;
+	calculate_button.textContent = "Calculate";
 }
 
 // Fills the badges with data retrieved from explorer API every 100 seconds (avg block time)
